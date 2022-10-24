674e2d1aaf81a7c9310a5e827fed8257ba8fbcfa
minjun <hitobi1014@gmail.com>
2022-10-24 21:03:44 +0900

    연산자

---
+++ src/main/java/chapter03/Operator.java
@@ -0,0 +1,14 @@
+package chapter03;
+
+public class Operator {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 5;
+
+        System.out.println(i++);
+        System.out.println(++j);
+
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
